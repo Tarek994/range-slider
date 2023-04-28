e738bf01c8568e9e25c16c25f5913e4cb14303f2
Tarek994 <ghali.tarek.123@hotmail.com>
2023-04-29 01:54:17 +0300

    feat(index.js): add new elements to the body

--- src/index.js
+++ src/index.js
@@ -8,10 +8,21 @@ function rangeSlider() {
   const input = document.createElement("input")
   input.type = "range"
 
+  const bar = document.createElement("div")
+  bar.classList.add("bar")
+
+  const fill = document.createElement("div")
+  fill.classList.add("fill")
+
+  const ruler = document.createElement("div")
+  ruler.classList.add("ruler")
+
+  bar.append(ruler, fill)
+
   const style = document.createElement("style")
   style.textContent = get_theme()
-  shadow.append(style, input)
-  return el
+  shadow.append(style, input, bar)
+  return el;
 }
 
 function get_theme() {
